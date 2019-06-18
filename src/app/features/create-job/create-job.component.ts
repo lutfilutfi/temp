391b124664084b838e5e5d198f92fcb8888a157d
ash alice <achuaswathi920@gmail.com>
2019-06-18 12:26:40 +0530

    Second last Commit

--- src/app/features/create-job/create-job.component.ts
+++ src/app/features/create-job/create-job.component.ts
@@ -13,110 +13,111 @@ export class CreateJobComponent implements OnInit {
   fal: number;
   // f:number;
   desci: String;
-  ck:boolean;
+  ck: boolean;
   vac: vacancy;
-  buuu:vacancy;
+  buuu: vacancy;
   // prSkills : any[];
-  prSkills : Array<object> = [];
-  @ViewChild('frm') form:NgForm;
-  @ViewChild('in') in:ElementRef;
-  @ViewChild('role') role:ElementRef;
-  @ViewChild('bu') bu:ElementRef;
-  @ViewChild('ps') ps:ElementRef;
-  @ViewChild('exp') exp:ElementRef;
-  @ViewChild('loc') loc:ElementRef;
-  @ViewChild('ed') ed:ElementRef;
-  @ViewChild('vc') vc:ElementRef;
-  @ViewChild('bh') bh:ElementRef;
-  @ViewChild('hm') hm:ElementRef;
-  @ViewChild('bn') bn:ElementRef;
-  @ViewChild('jd') jd:ElementRef;
-  @ViewChild('rc') rc:ElementRef;
+  prSkills: Array<object> = [];
+  @ViewChild('frm') form: NgForm;
+  @ViewChild('in') in: ElementRef;
+  @ViewChild('role') role: ElementRef;
+  @ViewChild('bu') bu: ElementRef;
+  @ViewChild('ps') ps: ElementRef;
+  @ViewChild('exp') exp: ElementRef;
+  @ViewChild('loc') loc: ElementRef;
+  @ViewChild('ed') ed: ElementRef;
+  @ViewChild('vc') vc: ElementRef;
+  @ViewChild('bh') bh: ElementRef;
+  @ViewChild('hm') hm: ElementRef;
+  @ViewChild('bn') bn: ElementRef;
+  @ViewChild('jd') jd: ElementRef;
+  @ViewChild('rc') rc: ElementRef;
 
   constructor(private serverService: ServerServices) {
     this.fal = 0;
     this.desci = "Trial";
     this.ck = true;
+    this.validateForm();
     // this.f=0;
     // this.vac.buissnessUnit=['BSS','PES','DTS'],
     // console.log(this.vac.buissnessUnit)
     this.serverService.getDefaults()
-    .subscribe(
-      (response: any) => {
-        var t = response;
-        this.vac=new vacancy(t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7],t[8],t[9]);
-      },
-      (error:any) => { console.log("Error", error) }
+      .subscribe(
+        (response: any) => {
+          var t = response;
+          this.vac = new vacancy(t[0], t[1], t[2], t[3], t[4],null,null,null,null,null,null);
+        },
+        (error: any) => { console.log("Error", error) }
       )
-      // this.buuu = this.vac.buissnessUnit;  
-    }
-    
-    ngOnInit() {}
-    
-    onSubmit(){
-      // this.p=this.ps.nativeElement.value,
-
-      // console.log(JSON.stringify(this.ps));
-      this.buuu=new vacancy(
-        this.bu.nativeElement.value,
-        // this.ps.nativeElement.value,
-        this.prSkills,
-        // this.p,
-        this.exp.nativeElement.value,
-        this.vc.nativeElement.value,
-        this.bh.nativeElement.value,
-        this.hm.nativeElement.value,
-        this.bn.nativeElement.value,
-        this.jd.nativeElement.value,
-        this.loc.nativeElement.value,
-        this.rc.nativeElement.value,
-        // this.
-        )
-        // console.log(this.buuu.primarySkills);
-        console.log(this.buuu);
-      this.serverService.saveJobs(this.buuu)
-        .subscribe(
-          (response: any) => { console.log("response") },
-          (error: any) => { console.log(error) }
-        );
-        // this.form.resetForm();
-    }
-
-    onReset(){
-      this.form.resetForm();
-      this.ck=true;
-    }
-
-    validateForm(){
-      if (this.form.valid ) {
-        return this.ck= false;
-      }
-    }
-
-    onClick(r: ElementRef){
-      this.fal = 1;
-      // return this.vac.jobDescription[]
-    }
-
-    onAddSkill(){
-      // console.log(this.in.nativeElement.value);
-      const v=this.in.nativeElement.value;
-      // if ()
-      // this.prSkills.forEach(p => {
-      //   if (v!=p){
-          this.prSkills.push(v);
-      //   }
-      // });
-      // this.f=1
-    }
-
-    onRemove(ind: number){
-      // this.prSkills.slice(i,2);
-      delete this.prSkills[ind];
-      this.prSkills.sort();
-      this.prSkills=this.prSkills.filter((val)=>{return val!=empty})
-      console.log(this.prSkills)
-      // this.f=1;
-    }
-
+    // this.buuu = this.vac.buissnessUnit;  
   }
+
+  ngOnInit() { }
+
+  onSubmit() {
+    // this.p=this.ps.nativeElement.value,
+
+    // console.log(JSON.stringify(this.ps));
+    this.buuu = new vacancy(
+      this.bu.nativeElement.value,
+      // this.ps.nativeElement.value,
+      this.role.nativeElement.value,
+      // this.p,
+      this.exp.nativeElement.value,
+      this.vc.nativeElement.value,
+      this.bh.nativeElement.value,
+      this.prSkills,
+      this.hm.nativeElement.value,
+      this.bn.nativeElement.value,
+      this.loc.nativeElement.value,
+      this.rc.nativeElement.value,
+      this.jd.nativeElement.value,
+      // this.
+    )
+    // console.log(this.buuu.primarySkills);
+    console.log(this.buuu);
+    this.serverService.saveJobs(this.buuu)
+      .subscribe(
+        (response: any) => { console.log("response") },
+        (error: any) => { console.log(error) }
+      );
+    // this.form.resetForm();
+  }
+
+  onReset() {
+    this.prSkills=[];
+    // this.f=9;
+    // console.log(this.f);
+    // this.form.resetForm();
+    this.ck = true;
+  }
+
+  validateForm() {
+    // if (this.form.touched) {
+      // return this.ck = false;
+    // }
+    
+  }
+
+  onAddSkill() {
+    // console.log(this.in.nativeElement.value);
+    const v = this.in.nativeElement.value;
+    // if ()
+    // this.prSkills.forEach(p => {
+    //   if (v!=p){
+    this.prSkills.push(v);
+    //   }
+    // });
+    // this.f=1
+  }
+
+  onRemove(ind: number) {
+    // this.prSkills.slice(i,2);
+    delete this.prSkills[ind];
+    this.prSkills.sort();
+    this.prSkills = this.prSkills.filter((val) => { return val != empty })
+    console.log(this.prSkills)
+    // this.f=1;
+  }
+
+}
--- src/app/shared/vacancy.model.ts
+++ src/app/shared/vacancy.model.ts
@@ -1,13 +1,14 @@
 export class vacancy{
     constructor(public buissnessUnit:String[], 
+        public jobRole: String[], 
+        public buHead: String[], 
+        public band: String[],
+        public location: String[], 
         public primarySkills :object[], 
         public Experience: String[], 
         public vacancies: String[], 
-        public buHead: String[], 
         public hiringManager: String[], 
-        public band: String[],
-        public jobDescription: String[], 
-        public location: String[], 
-        public recruiter: String[]
+        public recruiter: String[],
+        public jobDescription : String[]
         ){} 
 }
